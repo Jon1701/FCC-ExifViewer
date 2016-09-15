ef5eed4ce8fb9100fc50dd3f58272bf9b6a0aee4
Jon Balon <development@jonbalon.com>
2016-09-15 16:07:44 -0400

    Added placeholder <Jumbotron/> content

--- src/js/App/Jumbotron.jsx
+++ src/js/App/Jumbotron.jsx
@@ -12,7 +12,18 @@ export default class Jumbotron extends React.Component {
   render() {
     return (
       <div id="jumbotron">
-        Jumbotron
+        <h1>What is Metadata?</h1>
+        <p>
+          Metadata is data which provides information about other data.
+        </p>
+
+        <p>
+          Files such as pictures and documents contain metadata, such as the file size, location data, person who wrote the document, etc.
+        </p>
+
+        <p>
+          This tool will tell you the file size of any file you upload, provided that the file is less than 50MB in size.
+        </p>
       </div>
     )
   };// End Component Render.
