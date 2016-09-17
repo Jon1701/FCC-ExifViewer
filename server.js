7f6cc5f9097c032179afd72e47f9ef2531d40189
Jon Balon <development@jonbalon.com>
2016-09-17 16:03:15 -0400

    Added file size calculation

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var express = require('express');
 var multer  = require('multer');
 var app = express();
 
+var fs = require('fs');       // Filesystem.
 var del = require('delete');  // Deletes files.
+var path = require('path');
 
 ////////////////////////////////////////////////////////////////////////////////
 // Multer.
@@ -39,19 +41,30 @@ app.post('/upload', upload.single('uploads'), function(req, res, next) {
   // Get the full path and filename of the file which was just uploaded.
   var filePath = req.file.path;
 
+  // Data containing filesize and EXIF.
+  var data = {};
+
   var ExifImage = require('exif').ExifImage;
 
   try {
     new ExifImage({ image: filePath}, (error, exifData) => {
 
-      // If no EXIF data could be found, send errors to error route.
-      if (error) { return next(error); };
+      // Get filesize and add it to data variable.
+      data['fileSize'] = fs.statSync(filePath)['size'];
+
+      // If an error occurs, set exif property to null.
+      // If no error, store exif data.
+      if (error) {
+        data['exif'] = null;
+      } else {
+        data['exif'] = exifData.exif;
+      }
 
       // Delete uploaded file.
       deleteFile(filePath);
 
-      // Return EXIF data to the client.
-      res.json(exifData.exif);
+      // Send EXIF and file size info to client.
+      res.json(data);
 
     });
   } catch (error) {
@@ -63,9 +76,6 @@ app.post('/upload', upload.single('uploads'), function(req, res, next) {
     return next(error);
 
   };
-
-  // Delete uploaded file.
-  deleteFile(filePath);
 
 });
 
