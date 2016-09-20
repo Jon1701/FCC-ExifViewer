4bf8ccc3086fd02775c7aba58eb4c4478146a4e9
Jon Balon <development@jonbalon.com>
2016-09-20 15:17:16 -0400

    Added classes to toggle visibility

--- src/js/App/TabLayout/ResultsExif.jsx
+++ src/js/App/TabLayout/ResultsExif.jsx
@@ -102,18 +102,15 @@ export default class ResultsExif extends React.Component {
             {tagValue}
           </div>
         </div>
-      );
-    });
+      ); // End row creation and storing.
+
+    }); // End iteration.
 
     return results;
   };
 
   // Component Render.
   render() {
-
-    var myClasses = classNames({
-      'hidden': this.props.tabId != this.props.currentTab
-    });
 
     // Sanitize raw exif data.
     // Rewrite field names using natural language,
@@ -123,10 +120,40 @@ export default class ResultsExif extends React.Component {
     // Create table rows and cells using sanitized Exif data.
     var rows = this.createRows(newExifData);
 
+    // Classes to toggle display of the component in its entirety.
+    //
+    // This component should be displayed only when the tab ID matches
+    // the current tab ID.
+    var classesDisplayResults = classNames({
+      'hidden': this.props.tabId != this.props.currentTab
+    });
+
+    // Classes to toggle display of the data table.
+    //
+    // If this.props.data is undefined, then no Exif data was sent,
+    // and the table should be hidden
+    var classesDisplayTable = classNames({
+      'hidden': typeof(this.props.data) == 'undefined',
+      'tbl': true
+    });
+
+    // Classes to toggle displaying of No Data Available message.
+    //
+    // If this.props.data is undefined, this means no Exif data
+    // was passed down. Therefore this div must not be hidden when
+    // no Exif data is provided.
+    var classesNoDataAvailable = classNames({
+      'hidden': typeof(this.props.data) != 'undefined',
+      'text-center': true
+    });
+
     return (
-      <div className={myClasses}>
-        <div className="tbl">
+      <div className={classesDisplayResults}>
+        <div className={classesDisplayTable}>
           {rows}
+        </div>
+        <div className={classesNoDataAvailable}>
+          No data available
         </div>
       </div>
     )
