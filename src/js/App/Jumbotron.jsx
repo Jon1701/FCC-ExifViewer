343c9cf287ca1b866014f43bc6723180a0e5b0e0
Jon Balon <development@jonbalon.com>
2016-09-20 17:11:20 -0400

    Changed Jumbotron text

--- src/js/App/Jumbotron.jsx
+++ src/js/App/Jumbotron.jsx
@@ -12,17 +12,9 @@ export default class Jumbotron extends React.Component {
   render() {
     return (
       <div id="jumbotron" className="jumbotron boxshadow">
-        <h1>What is Metadata?</h1>
+        <h1>What is Exif?</h1>
         <p>
-          Metadata is data which provides information about other data.
-        </p>
-
-        <p>
-          Files such as pictures and documents contain metadata, such as the file size, location data, person who wrote the document, etc.
-        </p>
-
-        <p>
-          This tool will tell you the file size of any file you upload, provided that the file is less than 50MB in size.
+          <span className="bold">Exchangeable Image File Format</span> is a standard for embedding information in photos. Such included information could be the model of the camera, aperture, ISO, shutter speed, and possibly even the location the photo was taken.
         </p>
       </div>
     )
